26e13dde7abcece485e2b3f80e502964b2ea7b3a
ryanascherr <ryanascherr@gmail.com>
2021-05-31 14:14:17 -0400

    Minor changes.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,28 +3,28 @@ const { Post, User, Comment } = require('../models');
 // const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-    // try {
-    //   // Get all projects and JOIN with user data
-    //   const postData = await Post.findAll({
-    //     include: [
-    //       {
-    //         model: User,
-    //         attributes: ['name'],
-    //       },
-    //     ],
-    //   });
+    try {
+      // Get all projects and JOIN with user data
+      const postData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
   
-    //   // Serialize data so the template can read it
-    //   const posts = postData.map((project) => project.get({ plain: true }));
+      // Serialize data so the template can read it
+      const posts = postData.map((project) => project.get({ plain: true }));
   
-    //   // Pass serialized data and session flag into template
-    //   res.render('homepage', { 
-    //     posts, 
-    //     logged_in: req.session.logged_in 
-    //   });
-    // } catch (err) {
-    //   res.status(500).json(err);
-    // }
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        posts, 
+        // logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
     res.render('homepage');
 });
 
