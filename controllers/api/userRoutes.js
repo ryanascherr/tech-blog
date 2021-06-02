5979786fd63f14042b0a01d891757b2ffe89aaf2
ryanascherr <ryanascherr@gmail.com>
2021-06-02 10:51:30 -0400

    Tried some more route stuff.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,38 +1,39 @@
 const router   = require('express').Router();
 const { User } = require('../../models');
-const withAuth = require('../../utils/auth');
+// const withAuth = require('../../utils/auth');
 
 //Logs in a current user by checking entered data against data from the database
 router.post('/login', async (req, res) => {
-    try {
-      const userData = await User.findOne({ where: { username: req.body.username } });
-  
-      if (!userData) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect username or password, please try again' });
-        return;
-      }
-  
-      const validPassword = await userData.checkPassword(req.body.password);
-  
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect username or password, please try again' });
-        return;
-      }
-  
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
-        console.log("i'm logged in")
-        res.json({ user: userData, message: 'You are now logged in!' });
-      });
-  
-    } catch (err) {
-      res.status(400).json(err);
+  try {
+    const userData = await User.findOne({ where: { username: req.body.username } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
     }
-  });
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      // req.session.user_name = userData.name,
+      req.session.logged_in = true;
+      console.log("i'm logged in")
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,16 @@
 const submitBtn = $(".login-signup-btn");
 
-submitBtn.click(function(e) {
+const loginhandler = async (e) => {
     // Stop the browser from submitting the form so we can do so with JavaScript
     e.preventDefault();
-  
     // Gather the data from the form elements on the page
     const username = document.querySelector('.username-login').value.trim();
     const password = document.querySelector('.password-login').value.trim();
     console.log(username + " " + password);
     if (username && password) {
       // Send the username and password to the server
-      const response = fetch('/api/users/login', {
+      console.log("Success!");
+      const response = await fetch('/api/users/login', {
         method: 'POST',
         body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
@@ -22,4 +22,6 @@ submitBtn.click(function(e) {
         alert('Failed to log in');
       }
     }
-});
+};
+
+submitBtn.click(loginhandler);
