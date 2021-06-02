bfeb41644f774fb4cb2ed86221d4ac1243057012
ryanascherr <ryanascherr@gmail.com>
2021-06-02 13:50:00 -0400

    Added dashboard and displayed all of user's posts.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -35,14 +35,32 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.get('/dashboard', (req, res) => {
-  //If the user is already logged in, redirect them to the homepage (homepage.handlebars)
-  if (!req.session.logged_in) {
-    res.redirect('/');
-    return;
+router.get('/dashboard', async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    console.table(user);
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
   }
-  res.render('dashboard');
 });
+  //If the user is already logged in, redirect them to the homepage (homepage.handlebars)
+//   if (!req.session.logged_in) {
+//     res.redirect('/');
+//     return;
+//   }
+//   res.render('dashboard');
+// });
 
 router.get('/post/:id', async (req, res) => {
   try {
